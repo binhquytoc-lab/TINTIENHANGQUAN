a30d264e555b52df2abd5f4e55e36569d567eff2
binhquytoc-lab <binhquytoc@gmail.com>
2026-07-09 15:52:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ menu = {
     "Đồ ăn": {
         "Pizza Hải Sản": 150000, "Mì Ý Bò Bằm": 95000, "Burger Gà": 65000,
         "Salad Trộn": 50000, "Bít tết Bò Mỹ": 250000, "Sườn nướng BBQ": 180000,
-        "Cánh gà chiên mắm": 75000, "Lẩu Thái hải sản": 350000
+        "Cánh gà chiên mắm": 75000,"Lẩu cá diêu hồng":200000, "Lẩu Thái hải sản": 300000
     },
     "Thức uống": {
         "Coca Cola": 20000, "Trà Đào Cam Sả": 35000, "Cà Phê Sữa": 25000,
